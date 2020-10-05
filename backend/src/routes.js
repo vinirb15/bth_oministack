621034329286b446049381780b022cb3560a543e
vbueno <viniciusbueno15@gmail.com>
2020-10-05 09:53:22 -0300

    routes

---
+++ backend/src/routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const routes = express.Router();
+const OngController = require('./app/controllers/OngController');
+const IncidentController = require('./app/controllers/IncidentController');
+const ProfileController = require('./app/controllers/ProfileController');
+const SessionController = require('./app/controllers/SessionController');
+
+const authMiddleware = require('./app/middlewares/auth');
+
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
+
+routes.use(authMiddleware);
+
+
+
+module.exports = routes;
