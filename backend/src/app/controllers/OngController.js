69d3d620b6556a3ad118e405528c9f668601da1d
vbueno <viniciusbueno15@gmail.com>
2020-10-05 09:52:12 -0300

    controllers

---
+++ backend/src/app/controllers/OngController.js
@@ -0,0 +1,28 @@
+const connection = require('../../database/index');
+const crypto = require('crypto');
+
+
+module.exports = {
+    async index (request, response) {
+        const ongs = await connection('ongs').select('*');
+        
+        return response.json(ongs);
+    },
+
+    async create(request, response) {
+        const /*data*/ { name, email, whatsapp, city, uf } = request.body;
+
+        const id = crypto.randomBytes(4).toString('HEX');
+
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        })
+
+        return response.json({ id });
+    }
+}
