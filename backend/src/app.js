3d1feae3ed1a314ba12bace80d39c78bdce27afc
vbueno <viniciusbueno15@gmail.com>
2020-10-13 09:36:39 -0300

    celebrate authentication

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,24 +1,14 @@
 const express = require('express');
-var cors = require('cors')
+const cors = require('cors');
 const routes = require('./routes');
+const { errors } = require('celebrate');
+
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(routes);
-
-//notFound
-app.use((req, res, next) => {
-    const error = new Error('Not Found')
-    error.status = 404
-    next(error)
-})
-
-//catch all
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({ error: error.message })
-})
+app.use(errors());
 
 app.listen(3333, () => {
     var dtNow = new Date;
